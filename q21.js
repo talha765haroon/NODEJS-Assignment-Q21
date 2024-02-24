462470e9868893ebf33ec5142d7485cb4dde851d
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 04:18:23 +0500

    Add files via upload

---
+++ q21.js
@@ -0,0 +1,5 @@
+"use strict";
+/*Ques # 21:
+Let think ofsomething you could store in a Typescript Object.Write a program that creates object containing these items*/
+let person = { Name: "Talha", fatherName: " Haroon", Age: 23 };
+console.log(person);
---
+++ q21.ts
@@ -0,0 +1,6 @@
+/*Ques # 21:
+Let think ofsomething you could store in a Typescript Object.Write a program that creates object containing these items*/
+
+
+let person : {Name : string, fatherName : string, Age : number} = {Name : "Talha", fatherName : " Haroon", Age : 24}
+console.log(person);
